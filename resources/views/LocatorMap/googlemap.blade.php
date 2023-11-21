8003d5a6fcee1b94c0d9cb706184d1971d56c46b
Lloyd P Paliuanan <lloydpazko@gmail.com>
2023-11-21 22:50:33 +0800

    added htmlcode googlemap

--- resources/views/LocatorMap/googlemap.blade.php
+++ resources/views/LocatorMap/googlemap.blade.php
@@ -1,13 +1,121 @@
 <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
+<html style="font-size: 16px;" lang="en"><head>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-    <h1>I LOVE DICK</h1>
+    <meta charset="utf-8">
+    <meta name="keywords" content="Evacuation locator SYSTEM, Contact Us">
+    <meta name="description" content="">
+    <title>Maps</title>
+    <link rel="stylesheet" href="{{ asset('nicepage.css') }}" media="screen">
+<link rel="stylesheet" href="{{ asset('Maps.css') }}" media="screen">
+    <script class="u-script" type="text/javascript" src="jquery.js" defer=""></script>
+    <script class="u-script" type="text/javascript" src="nicepage.js" defer=""></script>
+    <meta name="generator" content="Nicepage 6.0.3, nicepage.com">
+    <link id="u-theme-google-font" rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:100,100i,300,300i,400,400i,500,500i,700,700i,900,900i|Open+Sans:300,300i,400,400i,500,500i,600,600i,700,700i,800,800i">
 
-</body>
-</html>
+
+
+
+    <script type="application/ld+json">{
+		"@context": "http://schema.org",
+		"@type": "Organization",
+		"name": "",
+		"logo": "images/2560px-National_Disaster_Risk_Reduction_and_Management_Council_NDRMMC.svg.png"
+}</script>
+    <meta name="theme-color" content="#478ac9">
+    <meta property="og:title" content="Maps">
+    <meta property="og:type" content="website">
+  <meta data-intl-tel-input-cdn-path="intlTelInput/"></head>
+  <body data-home-page="Maps.html" data-home-page-title="Maps" data-path-to-root="./" data-include-products="false" class="u-body u-xl-mode" data-lang="en"><header class="u-clearfix u-header u-header" id="sec-d09c"><div class="u-clearfix u-sheet u-valign-middle u-sheet-1">
+        <nav class="u-offcanvas u-menu-1">
+          <div class="menu-collapse" style="font-size: 1rem; letter-spacing: 0px;">
+            <a class="u-button-style u-custom-left-right-menu-spacing u-custom-padding-bottom u-custom-top-bottom-menu-spacing u-nav-link u-text-active-palette-1-base u-text-hover-palette-2-base" href="#">
+              <svg class="" viewBox="0 0 24 24"><use xmlns:xlink="" xlink:href="#menu-hamburger"></use></svg>
+              <svg class="" version="1.1" id="" viewBox="0 0 16 16" x="0px" y="0px" xmlns:xlink="" xmlns=""><g><rect y="1" width="16" height="2"></rect><rect y="7" width="16" height="2"></rect><rect y="13" width="16" height="2"></rect>
+</g></svg>
+            </a>
+          </div>
+          @if (Route::has('login'))
+          <div class="u-custom-menu u-nav-container">
+            <ul class="u-nav u-unstyled u-nav-1">
+                <li class="u-nav-item">
+                    <a class="u-button-style u-nav-link u-text-active-palette-1-base u-text-hover-palette-2-base" rel="nofollow" style="padding: 10px 20px;" href="{{ url('/dashboard') }}">Home</a>
+</li>
+@auth
+<li class="u-nav-item">
+    <a href="{{ route('LocatorMap.googlemap') }}" class="u-button-style u-nav-link u-text-active-palette-1-base u-text-hover-palette-2-base" rel="nofollow" style="padding: 10px 20px;">Maps</a>
+</li>
+@else
+<li class="u-nav-item">
+    <a class="u-button-style u-nav-link u-text-active-palette-1-base u-text-hover-palette-2-base" rel="nofollow" style="padding: 10px 20px;" href="{{ route('login') }}">Login</a>
+</li>
+@if (Route::has('register'))
+<li class="u-nav-item">
+    <a class="u-button-style u-nav-link u-text-active-palette-1-base u-text-hover-palette-2-base" rel="nofollow" style="padding: 10px 20px;" href="{{ route('register') }}">Register</a>
+</li>
+@endif
+@endauth
+
+@endif
+</ul>
+          </div>
+          <div class="u-custom-menu u-nav-container-collapse">
+            <div class="u-black u-container-style u-inner-container-layout u-opacity u-opacity-95 u-sidenav">
+              <div class="u-inner-container-layout u-sidenav-overflow">
+                <div class="u-menu-close"></div>
+                <ul class="u-align-center u-nav u-popupmenu-items u-unstyled u-nav-2"><li class="u-nav-item"><a class="u-button-style u-nav-link" rel="nofollow">Home</a>
+</li><li class="u-nav-item"><a class="u-button-style u-nav-link" rel="nofollow">Maps</a>
+</li><li class="u-nav-item"><a class="u-button-style u-nav-link" rel="nofollow">Login</a>
+</li><li class="u-nav-item"><a class="u-button-style u-nav-link" rel="nofollow">Register</a>
+</li></ul>
+              </div>
+            </div>
+            <div class="u-black u-menu-overlay u-opacity u-opacity-70"></div>
+          </div>
+        </nav>
+        <a href="https://nicepage.com/k/youtube-website-templates" class="u-image u-logo u-image-1" data-image-width="2560" data-image-height="1014">
+          <img src="images/2560px-National_Disaster_Risk_Reduction_and_Management_Council_NDRMMC.svg.png" class="u-logo-image u-logo-image-1">
+        </a>
+      </div></header>
+    <section class="u-align-center u-clearfix u-image u-shading u-section-1" src="" data-image-width="256" data-image-height="256" id="sec-02a7">
+      <div class="u-clearfix u-sheet u-sheet-1">
+        <h1 class="u-text u-text-default u-title u-text-1">Evacuation locator APP</h1>
+        <p class="u-large-text u-text u-text-default u-text-variant u-text-2">Power by Aparrianos.</p>
+        <a href="#" class="u-btn u-button-style u-palette-2-base u-btn-1">Read More</a>
+      </div>
+    </section>
+
+    <footer class="u-align-center u-clearfix u-footer u-grey-80 u-footer" id="sec-5e91"><div class="u-clearfix u-sheet u-sheet-1">
+        <p class="u-small-text u-text u-text-variant u-text-1">Powered by GoogleMaps</p>
+      </div></footer>
+    <section class="u-backlink u-clearfix u-grey-80">
+      <a class="u-link" href="https://nicepage.com/website-templates" target="_blank">
+        <span>Website Templates</span>
+      </a>
+      <p class="u-text">
+        <span>created with</span>
+      </p>
+      <a class="u-link" href="" target="_blank">
+        <span>Website Builder Software</span>
+      </a>.
+    </section>
+
+</body></html>
+
+<!--
+        <div class="relative sm:flex sm:justify-center sm:items-center min-h-screen bg-dots-darker bg-center bg-gray-100 dark:bg-dots-lighter dark:bg-gray-900 selection:bg-red-500 selection:text-white">
+            @if (Route::has('login'))
+                <div class="sm:fixed sm:top-0 sm:right-0 p-6 text-right z-10">
+                    @auth
+                        <a href="{{ url('/dashboard') }}" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Dashboard</a>
+                    @else
+                        <a href="{{ route('login') }}" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Log in</a>
+
+                        @if (Route::has('register'))
+                            <a href="{{ route('register') }}" class="ml-4 font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Register</a>
+                        @endif
+                    @endauth
+                </div>
+            @endif
+
+            -->
+
